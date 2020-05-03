ded2e0ed429c1e74c4fb96d2c55a7f9b1a613ab9
shafali03 <shaf03sm@gmail.com>
2020-05-03 18:21:07 +0100

    feat: Using axios to perform a get requests

--- src/context/products.js
+++ src/context/products.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-
+import axios from 'axios'
+import url from '../utils/URL'
 export const ProductContext = React.createContext()
 
 //Provider, Consumer, useContext()
@@ -8,6 +9,11 @@ function ProductProvider({ children }) {
   const [loading, setLoading] = (useState(false))
   const [products, setProducts] = (useState(false))
   const [featured, setFeatured] = (useState(false))
+
+  React.useEffect(() => {
+    axios.get(`${url}/products`).then(storeProducts => console.log(storeProducts))
+    return () => { }
+  })
 
   return (
     <ProductContext.Provider value={{ loading, products, featured }}>
