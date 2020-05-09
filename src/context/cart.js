e059e901fa81fcc2819ffae3c15d172abf22848a
shafali03 <shaf03sm@gmail.com>
2020-05-09 20:18:12 +0100

    feat: Added functionality for the total price for items

--- src/context/cart.js
+++ src/context/cart.js
@@ -9,7 +9,52 @@ function CartProvider({ children }) {
   const [total, setTotal] = React.useState(0)
   const [cartItems, setCartItems] = React.useState(0)
 
-  return <CartContext.Provider value={{ cart, total, cartItems }}>{children}</CartContext.Provider>
+  React.useEffect(() => {
+    // local storage
+    // accumulator = total
+    // current = cartItem
+    let newCartItems = cart.reduce((total, cartItem) => {
+      // console.log(total, cartItem)
+      return (total += cartItem.amount)
+    }, 0)
+
+    setCartItems(newCartItems)
+    // cart total
+    let newTotal = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount * cartItem.price)
+    }, 0)
+    // console.log(newCartItems)
+
+    newTotal = parseFloat(newTotal.toFixed(2))
+    setTotal(newTotal)
+  }, [cart])
+
+  // remove item
+  const removeItem = id => { }
+  // increase amount
+  const increaseAmount = id => { }
+  // decrease amount
+  const decreaseAmount = id => { }
+  // add to cart
+  const addToCart = id => { }
+  // clear cart
+  const clearCart = id => { }
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        total,
+        cartItems,
+        removeItem,
+        increaseAmount,
+        decreaseAmount,
+        addToCart,
+        clearCart
+      }}>
+      {children}
+    </CartContext.Provider>
+  )
 }
 
 export { CartContext, CartProvider }
