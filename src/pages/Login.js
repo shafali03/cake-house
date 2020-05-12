7828352d5cef396fd7e8fa5282d2e44435038796
shafali03 <shaf03sm@gmail.com>
2020-05-12 06:58:50 +0100

    feat: setting up login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,31 @@
 import React from 'react'
 
+// strapi function
+
+// handle user 
+import { useHistory } from 'react-router-dom'
+
 function Login() {
+  const history = useHistory()
+  // setup user context
+
+
+  // state values
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const [username, setUsername] = React.useState('default')
+  const [isMember, setIsMember] = React.useState(true)
+
+  let isEmpty = false;
+
+  const toggleMember = () => {
+
+  }
+
+  const handleSubmit = async (e) => {
+
+  }
+
   return (
     <div>
       <h1>Login page</h1>
