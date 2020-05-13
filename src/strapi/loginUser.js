dbc3aea703e6a8515b64ce53131cdbdde3d94651
shafali03 <shaf03sm@gmail.com>
2020-05-13 08:32:01 +0100

    feat: log in user in the strapi user found

---
+++ src/strapi/loginUser.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import url from '../utils/URL'
+
+async function loginUser({ email, password }) {
+  const response = await axios.post(`${url}/auth/local`, {
+    identifer: email,
+    password
+  })
+    .catch(error => console.log(error))
+  return response
+}
+
+export default loginUser
