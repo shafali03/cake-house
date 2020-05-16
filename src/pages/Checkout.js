6763fe735077a7c070c0c2d001e5e6db30a3c3f2
shafali03 <shaf03sm@gmail.com>
2020-05-16 11:11:54 +0100

    feat: Created checkout form

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -21,12 +21,54 @@ function Checkout(props) {
   }
   if (cart.length < 1) return <EmptyCart />
 
-
-
   return (
-    <div>
-      <h1>checkout page</h1>
-    </div>
+    <section className="section form">
+      <h2 className="section-title">checkout</h2>
+      <form className="checkout-form">
+        <h3>
+          order total : <span>£{total}</span>
+        </h3>
+        <div className="form-control">
+          <label htmlFor="name">name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={e => {
+              setName(e.target.value)
+            }}
+          />
+        </div>
+
+        {/* card element */}
+        <div className="stripe-input">
+          <label htmlFor="card-element">Card or Debit Card</label>
+          <p className="stripe-info">
+            Test using this card details : <span>4242 4242 4242</span>
+            <br />
+          enter any 5 digits for the zip code
+          <br />
+          enter any 3 digits for the CVC
+        </p>
+        </div>
+
+        {/* stripe error */}
+        {error && <p className="form-empty">{error}</p>}
+        {/* empty value */}
+        {isEmpty ? (
+          <p className="form-empty">please fill out name field</p>
+        ) : (
+            <button type="submit" onClick={handleSubmit}
+              className="btn btn=primary btn-block">
+              submit
+            </button>
+          )}
+
+
+
+
+      </form>
+    </section>
   )
 }
 
