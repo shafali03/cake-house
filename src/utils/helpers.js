352a9b07fa228cf7c8117cd7c97022b386e9e873
shafali03 <shaf03sm@gmail.com>
2020-06-02 05:35:18 +0100

    feat: Added utils folder with helper and url

---
+++ src/utils/helpers.js
@@ -0,0 +1,17 @@
+// import url from './URL'
+
+// flatten
+export function flattenProducts(data) {
+  return data.map(item => {
+    // cloudinary 
+    let image = (item.image && item.image.url) || null
+    // let image = item.image.url
+    return { ...item, image }
+  })
+}
+
+export function featuredProducts(data) {
+  return data.filter(item => {
+    return item.featured === true;
+  })
+}
